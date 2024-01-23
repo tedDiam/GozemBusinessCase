27a114341d19da310bc0e356bb65481c28548a8f
tedDiam <sergediam@gmail.com>
2024-01-23 23:32:49 +0000

    add ktor dependency to network module

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -38,6 +38,13 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.koin.android)
+
+    implementation (libs.ktor.client.android)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.content.negotiation)
+    // Logging
+    implementation (libs.ktor.client.logging)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
