f0f997b2f05b50be53f32e8198ceb41bc26c0352
tedDiam <sergediam@gmail.com>
2024-01-26 22:08:06 +0000

    create the application class

--- app/src/main/java/com/sergediame/gozembusinesscase/GozemBusinessCaseApplication.kt
+++ app/src/main/java/com/sergediame/gozembusinesscase/GozemBusinessCaseApplication.kt
@@ -1,26 +1,29 @@
 package com.sergediame.gozembusinesscase
 
 import android.app.Application
+import com.sergediame.auth.authModule
+
+import com.sergediame.data.dataModule
 import com.sergediame.network.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
 
-class GozemBusinessCaseApplication:Application() {
+class GozemBusinessCaseApplication : Application() {
 
 
-        override fun onCreate() {
-            super.onCreate()
+    override fun onCreate() {
+        super.onCreate()
 
-            startKoin {
-                // Log Koin into Android logger
-                androidLogger()
-                // Reference Android context
-                androidContext(this@GozemBusinessCaseApplication)
-                // Load modules
-                modules(appModule, networkModule)
-            }
-
+        startKoin {
+            // Log Koin into Android logger
+            androidLogger()
+            // Reference Android context
+            androidContext(this@GozemBusinessCaseApplication)
+            // Load modules
+            modules(appModule, dataModule, authModule, networkModule)
         }
+
+    }
 
 }
