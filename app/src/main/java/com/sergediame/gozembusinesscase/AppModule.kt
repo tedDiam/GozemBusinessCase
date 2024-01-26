db0c7d8cc0d334aa62bf51f9a9ad464b52af7ae1
tedDiam <sergediam@gmail.com>
2024-01-26 22:10:35 +0000

    set up koin appModule

--- app/src/main/java/com/sergediame/gozembusinesscase/AppModule.kt
+++ app/src/main/java/com/sergediame/gozembusinesscase/AppModule.kt
@@ -1,7 +1,14 @@
 package com.sergediame.gozembusinesscase
 
+import com.sergediame.gozembusinesscase.auth.login.LoginViewModel
+import com.sergediame.gozembusinesscase.auth.register.RegistrationViewModel
+import com.sergediame.gozembusinesscase.home.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-
+    viewModel { MainActivityViewModel(get()) }
+    viewModel { LoginViewModel(get()) }
+    viewModel { RegistrationViewModel(get()) }
+    viewModel { HomeViewModel(get(), get()) }
 }
